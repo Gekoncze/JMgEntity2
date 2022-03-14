8dbb314af3b6d7452da5e80bf623fffae2063cf0
mg <gekoncze@seznam.cz>
2022-03-14 19:03:40 +0100

    added test for interface creation in entity class factory test

--- test/cz/mg/entity/services/factories/EntityClassFactoryTest.java
+++ test/cz/mg/entity/services/factories/EntityClassFactoryTest.java
@@ -2,7 +2,9 @@ package cz.mg.entity.services.factories;
 
 import cz.mg.annotations.classes.Test;
 import cz.mg.entity.common.TestEntityClass;
+import cz.mg.entity.common.TestEntityInterface;
 import cz.mg.entity.common.TestEntitySubClass;
+import cz.mg.entity.common.TestEntitySubInterface;
 import cz.mg.entity.utilities.EntityClass;
 import cz.mg.test.Assert;
 
@@ -11,13 +13,15 @@ public @Test class EntityClassFactoryTest {
         System.out.print("Running " + EntityClassFactoryTest.class.getSimpleName() + " ... ");
 
         EntityClassFactoryTest test = new EntityClassFactoryTest();
-        test.testCreate();
+        test.testCreateClass();
         test.testCreateSubClass();
+        test.testCreateInterface();
+        test.testCreateSubInterface();
 
         System.out.println("OK");
     }
 
-    private void testCreate() {
+    private void testCreateClass() {
         EntityClassFactory factory = EntityClassFactory.getInstance();
         EntityClass entityClass = factory.create(TestEntityClass.class);
 
@@ -36,6 +40,34 @@ public @Test class EntityClassFactoryTest {
 
         Assert.assertNotNull(entityClass);
         Assert.assertEquals(TestEntitySubClass.class, entityClass.getClazz());
+        Assert.assertEquals(3, entityClass.getFields().count());
+        Assert.assertNotNull(entityClass.getFields().get(0));
+        Assert.assertNotNull(entityClass.getFields().get(1));
+        Assert.assertNotNull(entityClass.getFields().get(2));
+        Assert.assertEquals("Bar", entityClass.getFields().get(0).getName());
+        Assert.assertEquals("Foo", entityClass.getFields().get(1).getName());
+        Assert.assertEquals("FooBar", entityClass.getFields().get(2).getName());
+    }
+
+    private void testCreateInterface() {
+        EntityClassFactory factory = EntityClassFactory.getInstance();
+        EntityClass entityClass = factory.create(TestEntityInterface.class);
+
+        Assert.assertNotNull(entityClass);
+        Assert.assertEquals(TestEntityInterface.class, entityClass.getClazz());
+        Assert.assertEquals(2, entityClass.getFields().count());
+        Assert.assertNotNull(entityClass.getFields().get(0));
+        Assert.assertNotNull(entityClass.getFields().get(1));
+        Assert.assertEquals("Bar", entityClass.getFields().get(0).getName());
+        Assert.assertEquals("Foo", entityClass.getFields().get(1).getName());
+    }
+
+    private void testCreateSubInterface() {
+        EntityClassFactory factory = EntityClassFactory.getInstance();
+        EntityClass entityClass = factory.create(TestEntitySubInterface.class);
+
+        Assert.assertNotNull(entityClass);
+        Assert.assertEquals(TestEntitySubInterface.class, entityClass.getClazz());
         Assert.assertEquals(3, entityClass.getFields().count());
         Assert.assertNotNull(entityClass.getFields().get(0));
         Assert.assertNotNull(entityClass.getFields().get(1));
